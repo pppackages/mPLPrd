cf21ea05c0eb96cb80892f6a9510b3e793666782
Edoardo Zanelli <145352287+edoardozanelli@users.noreply.github.com>
2025-12-09 16:42:47 +0100

    Update mPLP_functions.R

--- R/mPLP_functions.R
+++ R/mPLP_functions.R
@@ -1,23 +1,33 @@
 ######################################################################################
 ## Auxiliary functions for the R programs mPLPnp and mPLPrd
+## Authors: Giuseppe Cavaliere, Sílvia Gonçalves, Morten Ø. Nielsen, & Edoardo Zanelli
+## version: 1.01 (9.12.2025)
 ######################################################################################
 
 library(sandwich)
 
 # Kernel function 
 K <- function(u, kersel) {
-  kersel <- kersel[1]  # force scalar
   
-  result <- if (kernel=="epanechnikov" | kernel=="epa") {
+  result <- if (is.character(kersel) && length(kersel) == 1 &&
+                kersel %in% c("epanechnikov", "epa")) {
+    
     ifelse(abs(u) <= 1, 0.75 * (1 - u^2), 0)
-  } else if (kernel=="triangular" | kernel=="tri") {
+    
+  } else if (is.character(kersel) && length(kersel) == 1 &&
+             kersel %in% c("triangular", "tri")) {
+    
     ifelse(abs(u) <= 1, (1 - abs(u)), 0)
-  } else if (kernel=="uniform" | kernel=="uni") {
+    
+  } else if (is.character(kersel) && length(kersel) == 1 &&
+             kersel %in% c("uniform", "uni")) {
+    
     ifelse(abs(u) <= 1, 0.5, 0)
+    
   } else {
-    warning("Unknown kernel type. Returning 0.")
-    0
+    stop("Invalid kernel specification.")
   }
+  
   return(as.numeric(result))  
 }
 
@@ -38,7 +48,7 @@ Gamma.p.inv <- function(u,h,p,kersel) {
   for (i in 1:(p+1)) {
     X[,i] <- (u)^(i-1)
   }
-  K.X <- K(u = u, kersel = kersel)/(h*length(u))
+  K.X <- K(u = u, kersel = kersel)/(h*n)
   nonzero_indices <- which(K.X != 0) 
   X <- X[nonzero_indices, ]
   K.X <- K.X[nonzero_indices]
@@ -51,6 +61,14 @@ LP.est <- function(y,x,x0,h,p,v,kersel) {
   
   n <- length(x)
   X <- matrix(NA, nrow = n, ncol = p+1)
+  u <- (x-x0)/h
+  for (i in 1:(p+1)) {
+    X[,i] <- (u)^(i-1)
+  }
+  K.X <- K(u = u, kersel = kersel)/(h*n)
+  nonzero_indices <- which(K.X != 0) 
+  X <- X[nonzero_indices, ]
+  K.X <- K.X[nonzero_indices]
   
   bwcheck <- 21
   if (!is.null(bwcheck)) {
@@ -58,10 +76,14 @@ LP.est <- function(y,x,x0,h,p,v,kersel) {
     h     <- max(h, bw.min)
   }
   
-  Gamma.inv <- Gamma.p.inv(u=(x-x0)/h, h=h, p=p, kersel=kersel) 
+
+  
+  Gamma.inv <- Gamma.p.inv(u=u, h=h, p=p, kersel=kersel) 
+  
+  
   e1 <- numeric(p+1)
   e1[v+1] <- 1
-  ghat <- factorial(v)*e1%*%Gamma.inv%*%crossprod(X*K.X,y)/(n*(h^v))
+  ghat <- factorial(v)*e1%*%Gamma.inv%*%crossprod(X*K.X,y[nonzero_indices])/(n*(h^v))
   return(ghat)
 }
 
@@ -161,6 +183,10 @@ CCT.se <- function(x,x0,h,p,kersel,epshat) {
 qrXXinv = function(x, ...) {
   chol2inv(chol(crossprod(x)))
 }
+
+
+
+
 
 
 
